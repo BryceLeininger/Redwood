25fb9f54c6d78c0b1f2b9181db6112ee841e3bd7
BryceLeininger <b.lenny4@gmail.com>
2026-03-27 18:56:18 -0700

    Create service-worker.js

---
+++ agent_factory/subdivision_scout_panel/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "subdivision-scout-shell-v1";
+const APP_SHELL = [
+  new URL("./", self.location.href).toString(),
+  new URL("./manifest.webmanifest", self.location.href).toString(),
+  new URL("./scout/styles.css", self.location.href).toString(),
+  new URL("./scout/app.js", self.location.href).toString(),
+  new URL("./scout/assets/icon-192.png", self.location.href).toString(),
+  new URL("./scout/assets/icon-512.png", self.location.href).toString(),
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+  if (new URL(event.request.url).origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response.ok) {
+          return response;
+        }
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
